00d80d7bada7a49eac835314d73ca42842e8f8d3
Sasagamo <naoyasakamoto59@gmail.com>
2024-09-26 03:09:45 +0900

    新規登録出来るようにストロングパラメータを設定、usersモデルにバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :surname, presence: true, format: { with: /\A[^\x01-\x7E]+\z/}
+  validates :name, presence: true, format: { with: /\A[^\x01-\x7E]+\z/}
+  validates :surname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
+  validates :name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
+  validates :birthdate, presence: true
+
 end
