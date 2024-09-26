c4ee720df8806be50e58f9334b1776bcec47c735
Sasagamo <naoyasakamoto59@gmail.com>
2024-09-26 16:44:36 +0900

    Fakerの導入、設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname                      { "TestUser" }
-    email                         { "test@test.com" }
-    password                      { "test00" }
+    nickname                      { Faker::Name.unique.name }
+    email                         { Faker::Internet.unique.email }
+    password                      { Faker::Internet.password(min_length: 6) }
     password_confirmation         { password }
     surname                    { "山田" }
     name                     { "太郎" }
     surname_kana               { "ヤマダ" }
     name_kana                { "タロウ" }
-    birthdate                    { "2000-01-01" }
+    birthdate                    { Faker::Date.birthday(min_age: 20,max_age:60) }
 
   end
 end
