24d544375f31acec4fc7635528f810fd0dfae909
Sasagamo <naoyasakamoto59@gmail.com>
2024-09-29 03:16:43 +0900

    ActiveHashの実装
    プルダウンで選択肢を選べるように実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_payer
+  belongs_to :shipping_day
+  belongs_to :prefecture
+
+  with_options numericality: { other_than: 1 ,message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_payer_id
+    validates :shipping_day_id
+    validates :prefecture_id
+  end
 end
