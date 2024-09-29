39a51fadb94e5fad03aaf6fa4c67e7dee60f7de4
Sasagamo <naoyasakamoto59@gmail.com>
2024-09-30 00:54:36 +0900

    JavaScriptで手数料と販売利益が表示されるように実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+const price = () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const salesProfit = document.getElementById("profit");
+    addTaxDom.innerHTML = Math.floor(inputValue*0.1)
+    salesProfit.innerHTML = Math.floor(inputValue-(inputValue*0.1))
+
+  })
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
