559856c84fd9f93ed7e5b0f35a65def79b9170bd
Sasagamo <naoyasakamoto59@gmail.com>
2024-09-30 10:56:29 +0900

    テストコードのexampleの文章を微修正しました

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -43,13 +43,13 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
-      it 'priceが300未満だと出品できない' do
+      it 'priceが¥300未満だと出品できない' do
         @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
 
-      it 'priceが9999999を超えると出品できない' do
+      it 'priceが¥9,999,999を超えると出品できない' do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
@@ -61,31 +61,31 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
 
-      it 'category_idが1だと出品できない' do
+      it 'category_idが---だと出品できない' do
         @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
-      it 'condition_idが1だと出品できない' do
+      it 'condition_idが---だと出品できない' do
         @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
 
-      it 'shipping_fee_payer_idが1だと出品できない' do
+      it 'shipping_fee_payer_idが---だと出品できない' do
         @item.shipping_fee_payer_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee payer can't be blank")
       end
 
-      it 'prefecture_idが1だと出品できない' do
+      it 'prefecture_idが---だと出品できない' do
         @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
 
-      it 'shipping_day_idが1だと出品できない' do
+      it 'shipping_day_idが---だと出品できない' do
         @item.shipping_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping day can't be blank")
