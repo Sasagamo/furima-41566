ba0cc54d9db78510aae9b9331523c2a78978ecc1
Sasagamo <naoyasakamoto59@gmail.com>
2024-09-30 14:31:33 +0900

    商品出品機能 修正#1
    priceのintege形において不要だった正規表現のバリデーションを削除しました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,7 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/ }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
